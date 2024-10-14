efcea241d5c2f1708bb41fa3c211b4eca92c1eac
USUARIO <USUARIO@LAPTOP-7QC4HA27.home>
2024-10-14 18:41:54 +0200

    Test Construcción de Prendas. Closes #5

---
+++ MIMODA/src/mimoda/MIMODA.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mimoda;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class MIMODA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+PRENDA prenda1 = new PRENDA("urbano", "rojo", 38, "poliéster", "primavera", true);
+        System.out.println("Test 1: Constructor Paramétrico");
+        System.out.println(prenda1.toString());  
+        System.out.println();
+
+        PRENDA prenda2 = new PRENDA();
+        System.out.println("Test 2: Constructor por Defecto");
+        System.out.println(prenda2.toString());  
+        System.out.println();
+
+        
+        prenda1.setTipoEstilo("formal");
+        prenda1.setColorPrimario("azul");
+        prenda1.setTallaPrenda(42);
+        prenda1.setMaterial("seda");
+        prenda1.setTemporada("invierno");
+        prenda1.setDisponible(false);
+        System.out.println("Test 3: Modificar valores con Setters");
+        System.out.println(prenda1.toString());  
+        System.out.println();
+
+        
+        PRENDA prendaCopia = new PRENDA(prenda1);
+        System.out.println("Test 4: Constructor Copia");
+        System.out.println(prendaCopia.toString());  
+        System.out.println();
+    }
+}
